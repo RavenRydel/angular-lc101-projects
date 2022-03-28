297de9fbf6980f202e874079191c20d99c275424
Raven <rydela0106@gmail.com>
2022-03-28 16:36:48 -0500

    Finished examples, finished half of exercise, in progress

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -8,8 +8,47 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Exercises: Angular Lesson 3';
 
+
   color = 'green';
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+       let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+       rocketImage.style.left = movement;
+       this.width = this.width + 10000;
+    }
+  }
+  
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+    }
+  }
+
+  handleLand() {
+    window.alert('The shuttle is landing. Landing gear engaged.');{
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'The shuttle has landed.';
+    }
+  }
+
+  handleAbort() {
+    let result = window.confirm('Are you sure you want to abort the flight?');
+    if (result) {
+       this.color = 'red';
+       this.height = 0;
+       this.width = 0;
+       this.message = 'Mission aborted.';
+    }
+  }
+
+
 }
